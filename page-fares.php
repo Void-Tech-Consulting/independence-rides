00e95abdeaf7adf22819f2fa25bde0f1a2f034be
Justin Liu <justin789liu@gmail.com>
2020-12-11 12:44:10 -0500

    added fares page blue box

--- page-fares.php
+++ page-fares.php
@@ -2,6 +2,29 @@
   get_header(); 
 ?>
 
-<h1>hello sir</h1>
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Fares</title>
+    <meta name="Fares" content="Fares">
+    <link rel="stylesheet" href="fares.css">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div id="bluebox">
+        <!-- <ul id="introText"> -->
+            <div id="introText1">Prices vary based on location, time of day, and number of stops.</div>
+            <div id="introText2"></div>
+            <div id="introText3">If a driver is requested to wait longer than 15 minutes, a $20 per hour wait fee is charged.</div>
+        <!-- </ul> -->
+    </div>
+    <div id="introxText2"></div>
+</body>
+</html>
+
 
 <?php get_footer(); ?>
