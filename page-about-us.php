5bf49276a10bb4373fcdbc14102e932e858f265a
geonkim905 <geonkim@umich.edu>
2020-12-12 19:28:01 +0900

    create text section in about-us homepage

--- page-about-us.php
+++ page-about-us.php
@@ -2,6 +2,25 @@
   get_header(); 
 ?>
 
-<h1 class="header">About Us</h1>
+<style>@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');</style>
+
+<div class="about-us-text-box">
+  <div class="header-box">
+    <h1 class="header">Why we are here</h1>
+  </div>
+  <div class="line"></div>
+  <div class="paragraph-box">
+    <p class="about-us-paragraph">
+    We all tend to take the independence that comes from driving
+    for granted, until it’s gone. When that happens, our world 
+    suddenly seems small. This is exactly the plight of thousands 
+    of seniors and vision impaired adults in our communities across 
+    metropolitan Detroit.  Independence Rides is all about dignified 
+    transportation for seniors and vision impaired adults in the 
+    greater Detroit area. 
+    </p>
+  </div>
+</div>
+
 
 <?php get_footer(); ?>
