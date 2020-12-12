5735868da8c657144c3197b460ec7913a1ab8162
Justin Liu <justin789liu@gmail.com>
2020-12-12 13:47:41 -0500

    Removed height, changed id to class, added font, changed px -> rem

--- page-fares.php
+++ page-fares.php
@@ -11,20 +11,19 @@
     <title>Fares</title>
     <meta name="Fares" content="Fares">
     <link rel="stylesheet" href="fares.css">
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <div id="bluebox">
-        <!-- <ul id="introText"> -->
-            <div id="introText1">Prices vary based on location, time of day, and number of stops.</div>
-            <div id="introText2"></div>
-            <div id="introText3">If a driver is requested to wait longer than 15 minutes, a $20 per hour wait fee is charged.</div>
-        <!-- </ul> -->
+    <div class="blue-box">
+            <div class="intro-text">Prices vary based on location, time of day, and number of stops.</div>
+            <div class="intro-vertline"></div>
+            <div class="intro-text">If a driver is requested to wait longer than 15 minutes, a $20 per hour wait fee is charged.</div>
     </div>
-    <div id="introxText2"></div>
 </body>
 </html>
+
 
 
 <?php get_footer(); ?>
