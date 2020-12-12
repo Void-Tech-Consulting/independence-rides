ef1322a835b5660b2b3ccd4d35099381e8813ce5
llam16 <lydia.lam104@gmail.com>
2020-12-11 21:55:50 -0800

    Updated page-receiving-rides.php for top section

--- page-receiving-rides.php
+++ page-receiving-rides.php
@@ -2,6 +2,18 @@
   get_header(); 
 ?>
 
-<h1 class="receiving-rides-header">Receiving Rides</h1>
+</head>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div class="top-section">
+  <div class="top-info">
+    <h1 class="top-header">Let's go for a ride!</h1>
+    <button class="apply-button" type="button">Apply Now</button>
+  </div>
+</div>
+</body>
 
 <?php get_footer(); ?>
