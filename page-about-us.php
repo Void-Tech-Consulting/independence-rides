0ce1bdd29e022bf28b4f4012a921668029b46937
geonkim905 <geonkim@umich.edu>
2020-12-14 06:26:31 +0900

    update text section of about-us page

--- page-about-us.php
+++ page-about-us.php
@@ -5,12 +5,9 @@
 <style>@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');</style>
 
 <div class="about-us-text-box">
-  <div class="header-box">
-    <h1 class="header">Why we are here</h1>
-  </div>
-  <div class="line"></div>
-  <div class="paragraph-box">
-    <p class="about-us-paragraph">
+  <h1 class="header-text">Why we are here</h1>
+  <div class="header-line"></div>
+  <p class="about-us-paragraph">
     We all tend to take the independence that comes from driving
     for granted, until it’s gone. When that happens, our world 
     suddenly seems small. This is exactly the plight of thousands 
@@ -18,8 +15,7 @@
     metropolitan Detroit.  Independence Rides is all about dignified 
     transportation for seniors and vision impaired adults in the 
     greater Detroit area. 
-    </p>
-  </div>
+  </p>
 </div>
 
 
