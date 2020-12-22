f2a08feb0b704a219ed0296148cb9549cc550fde
Justin Liu <justin789liu@gmail.com>
2020-12-22 11:34:30 -0500

    add important information bullet points

--- page-fares.php
+++ page-fares.php
@@ -21,6 +21,27 @@
             <div class="intro-vertline"></div>
             <div class="intro-text">If a driver is requested to wait longer than 15 minutes, a $20 per hour wait fee is charged.</div>
     </div>
+    <div class="important-info">Important Information</div>
+    <ul id="info-list">
+      <li class="info-list-item">We make sure that the vehicle and all occupants are fully insured according to Michigan state law. </li>
+      <li class="info-list-item">All drivers are able to help carry groceries, and provide walking assistance to any rider who desires it. </li>
+      <li class="info-list-item">Independence Rides is a 501(c)(3) public charity to help keep rides affordable.</li>
+      <li class="info-list-item">This service is in collaboration with ITNAmerica, a national nonprofit senior transportation service. </li>
+    </ul>
+    <ul class="info-icon-list">
+      <li class="info-icon-box">
+        <div class="info-icon-image1"></div>
+        <div class="info-icon-text1"></div>
+      </li>
+      <li class="info-icon-box">
+        <div class="info-icon-image2"></div>
+        <div class="info-icon-text2"></div>
+      </li>
+      <li class="info-icon-box">
+        <div class="info-icon-image3"></div>
+        <div class="info-icon-text3"></div>
+      </li>
+    </ul>
 </body>
 </html>
 
