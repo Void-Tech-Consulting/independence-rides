cddfa8631d15f78f5b0a2da60f408b447f255e9c
Justin Liu <justin789liu@gmail.com>
2020-12-22 23:03:20 -0500

    add giving-rides page

---
+++ page-giving-rides.php
@@ -0,0 +1,45 @@
+<?php 
+  get_header(); 
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Giving Rides</title>
+    <meta name="Giving-Rides" content="Giving-Rides">
+    <link rel="stylesheet" href="giving-rides.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="important-info">Important Information</div>
+    <ul id="info-list">
+      <li class="info-list-item">We make sure that the vehicle and all occupants are fully insured according to Michigan state law. </li>
+      <li class="info-list-item">All drivers are able to help carry groceries, and provide walking assistance to any rider who desires it. </li>
+      <li class="info-list-item">Independence Rides is a 501(c)(3) public charity to help keep rides affordable.</li>
+      <li class="info-list-item">This service is in collaboration with ITNAmerica, a national nonprofit senior transportation service. </li>
+    </ul>
+    <ul class="info-icon-list">
+      <li class="info-icon-box">
+        <div class="info-icon-image1"></div>
+        <div class="info-icon-text1"></div>
+      </li>
+      <li class="info-icon-box">
+        <div class="info-icon-image2"></div>
+        <div class="info-icon-text2"></div>
+      </li>
+      <li class="info-icon-box">
+        <div class="info-icon-image3"></div>
+        <div class="info-icon-text3"></div>
+      </li>
+    </ul>
+</body>
+</html>
+
+
+
+<?php get_footer(); ?>
