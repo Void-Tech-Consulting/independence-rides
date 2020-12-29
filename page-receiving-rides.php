9e451f34b6429388916e151027e7b0cc05eab6aa
llam16 <lydia.lam104@gmail.com>
2020-12-28 19:17:21 -0800

    Implemented receiving rides steps timeline but not responsive

--- page-receiving-rides.php
+++ page-receiving-rides.php
@@ -14,6 +14,36 @@
     <button class="apply-button" type="button">Apply Now</button>
   </div>
 </div>
+<div class="steps-section">
+  <div class="steps-header">
+    <h2 class="steps-name">Recieving Rides</h2>
+    <div class="steps-underline"></div>
+  </div>
+  <div class="steps-body">
+    <div class="steps-image"></div>
+    <div class="steps-timeline">
+      <ul>
+        <li class="steps-joint-bullet-1">
+          <h3>Step One</h3>
+          <h4>Complete an application to ensure we have complete contact information.</h4>
+          <button class="steps-button" type="button">Application</button>
+        </liv>
+        <li class="steps-joint-bullet-2">
+          <h3>Step Two</h3>
+          <h4>Set up a membership for payment options.</h4>
+            <button class="steps-button" type="button">Membership</button>
+        </liv>
+        <li>
+          <h3>Step Three</h3>
+          <h4>Schedule a ride at least 24 hours in advance. 
+            Rides are normally available for any reason, but are limited to medical 
+            appointments and grocery shopping during the Coronavirus pandemic for 
+            your safety</h4>
+        </liv>
+      </ul>
+    </div>
+  </div>
+</div>
 </body>
 
 <?php get_footer(); ?>
