d0c1e66f3d34155742487ba26d09a9cdb2b5d49a
Ashique Rahman <ashique@umich.edu>
2020-12-29 12:28:48 -0500

    Early changes

---
+++ page-home.php
@@ -0,0 +1,17 @@
+<?php 
+  get_header(); 
+?>
+<section class="home-section-1">
+  <img class = "detSkyline" src = "<?php echo get_template_directory_uri();?>/images/detroit-skyline.jpg">
+  <div class="section-1-heading">
+    <h1 class="section-1-title">Independence Rides</h1>
+    <p class="title-text">A dignified transportation system for seniors and vision impaired adults in the greater Detroit area</p>
+    <div class="get-started-button">
+      <p>Get Started</p>
+    </div>
+  </div>
+</section>
+
+
+
+<?php get_footer(); ?>
