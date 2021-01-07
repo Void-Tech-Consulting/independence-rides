80f20a213430757dafa5b8f3622601da820586aa
Ashique Rahman <ashique@umich.edu>
2021-01-07 14:32:34 -0500

    early commit
    first commit, still needs some work

--- page-home.php
+++ page-home.php
@@ -2,7 +2,7 @@
   get_header(); 
 ?>
 <section class="home-section-1">
-  <img class = "detSkyline" src = "<?php echo get_template_directory_uri();?>/images/detroit-skyline.jpg">
+  <img class = "detSkyline" src = "<?php echo get_template_directory_uri();?>/images/homepage-cover-photo.png">
   <div class="section-1-heading">
     <h1 class="section-1-title">Independence Rides</h1>
     <p class="title-text">A dignified transportation system for seniors and vision impaired adults in the greater Detroit area</p>
@@ -10,8 +10,71 @@
       <p>Get Started</p>
     </div>
   </div>
+  <p class="section-1-bottom-text">Transportation freedom need not be a thing of the past.</p>
+</section>
+<div class=section-1-horizontal-line></div>
+<section class="info-section">
+    <div class="assistance-box">
+    <img class = "assistance-arms" src = "<?php echo get_template_directory_uri();?>/images/assistance-image.png">
+      <h1>Assistance Provided</h1>
+      <div class="assistance-text">
+        <p>Arm-through-arm and door-through-door assistance, help with canes, walkers, folding wheelchairs, grocery bags, and packages</p>
+      </div>
+    </div>
+    <div class="payment-box">
+      <h1>Payment</h1>
+      <div class="payment-text">
+        <p>Fares paid through our internal account system (no cash or cards needed), trading your unwanted car can help pay for rides, our nonprofit status keeps fares low</p>
+      </div>
+    </div>
+    <div class="availability-box">
+    <img class = "availability-clock" src = "<?php echo get_template_directory_uri();?>/images/availability-image.png">
+
+      <h1>Availability</h1>
+        <div class="availability-text">
+          <p>Rides available 24/7 for any reason. The current COVID-19 pandemic may drive some restrictions.</p>
+        </div>
+    </div>
+    <div class="drivers-box">
+      <h1>Drivers</h1>
+      <div class="drivers-text">
+        <p>Drivers are trained community volunteers who have passed criminal background and driving record checks.</p>
+      </div>
+
+    </div>
+
 </section>
 
+<section class="footer">
+  <div class="rides-contact">
+    <div class="footer-rides">
+      <h3 class="footer-heading">Independence Rides</h1>
+      <p class="ITN-footer">powered by ITNCountry</p>
+    </div>
+    <div class="footer-title">Contact Us</div>
+    <div class="footer-contact">
+      <p class="info-footer">CARL W. WELLBORN SR.<br>EXECUTIVE DIRECTOR<br>TEL: 313-618-1578<br>TEL: 313-618-3361<br>CARL@INDEPENDENCERIDES.COM</p>
+    </div>
+  </div>
+  <div class="footer-section">
+      <h3 class="footer-title">Our Mission </h3>
+      <p class="info-footer">Dignified Transportation for Seniors and Vision Impaired Adults</p>
+  </div>
+  <div class="footer-section">
+    <h3 class="footer-title">Information</h3>
+    <p class="info-footer">About Us<br>Receiving Rides<br>Fares</p>
+  </div>
+  <div class="footer-section">
+    <h3 class="footer-title">Get Involved</h3>
+    <p class="info-footer">Giving Rides<br>Ride Coordinator</p>
+  </div>
+  <div class="footer-section">
+    <h3 class="footer-title">Donate</h3>
+  </div>
+  <div class="footer-section">
+    <img class="transparency-seal" src = "<?php echo get_template_directory_uri();?>/images/seal-of-transparency.png">
+  </div>
 
+</section>
 
 <?php get_footer(); ?>
