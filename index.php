88a0c5b1d0b12ac025a164f180bc4cf92b8ab801
Justin Liu <justin789liu@gmail.com>
2021-01-16 17:30:21 -0500

    added order to fix mobile home page boxes

--- index.php
+++ index.php
@@ -26,24 +26,24 @@
 </iframe>
 <div class="info-icon-list info-icon-list-home">
       <div class="box-top">
-        <div class="info-icon-box-gray">
+        <div class="info-icon-box-gray light-arms-box">
           <img class = "info-icon-image" src = "<?php echo get_template_directory_uri();?>/images/light-arms.png">
           <div class="info-icon-text-subtitle">Assistance Provided</div>
           <div class="info-icon-text info-icon-text-home">Arm-through-arm and door-through-door assistance, help with canes, walkers, folding wheelchairs, grocery bags, and packages</div>
         </div>
-        <div class="info-icon-box-white">
+        <div class="info-icon-box-white dollar-box">
           <img class = "info-icon-image" src = "<?php echo get_template_directory_uri();?>/images/dollar-icon.png">
           <div class="info-icon-text-subtitle">Payment</div>
           <div class="info-icon-text info-icon-text-home">Fares paid through our internal account system (no cash or cards needed), trading your unwanted car can help pay for rides, our nonprofit status keeps fares low</div>
         </div>
       </div>
       <div class="box-bot">
-        <div class="info-icon-box-gray">
+        <div class="info-icon-box-gray light-clock-box">
           <img class = "info-icon-image" src = "<?php echo get_template_directory_uri();?>/images/light-clock.png">
           <div class="info-icon-text-subtitle">Availability</div>
           <div class="info-icon-text info-icon-text-home">Rides available 24/7 for any reason. The current COVID-19 pandemic may drive some restrictions.</div>
         </div>
-        <div class="info-icon-box-white">
+        <div class="info-icon-box-white wheel-box">
           <img class = "info-icon-image" src = "<?php echo get_template_directory_uri();?>/images/wheel-icon.png">
           <div class="info-icon-text-subtitle">Drivers</div>
           <div class="info-icon-text info-icon-text-home">Drivers are trained community volunteers who have passed criminal background and driving record checks.</div>
