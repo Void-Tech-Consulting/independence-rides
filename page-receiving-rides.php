2a0bd6d38cb13f7cb1e3e84577326eb280afaf98
llam16 <lydia.lam104@gmail.com>
2021-01-20 13:22:24 -0500

    Adding timeline section to giving rides page

--- page-receiving-rides.php
+++ page-receiving-rides.php
@@ -29,37 +29,35 @@ Any time of the day and for any reason we are here for you or your loved one. </
 
 <div class="steps-section">
   <div class="steps-content">
-  <div class="steps-header">
-    <h2 class="steps-name">Recieving Rides</h2>
-    <div class="steps-underline"></div>
-  </div>
-  <div class="steps-body">
-    <img class="steps-img"src="<?php echo get_template_directory_uri();?>/images/receiving-rides-steps.png"> 
-    <div class="steps-timeline">
-      <ul class="steps-timeline-list">
-        <li class="steps-bullet">
-          <h3 class="steps-subtitle">Step One</h3>
-          <h4 class="steps-sub-info">Complete an application to ensure we have complete contact information.</h4>
-          <button class="steps-button" type="button">Application</button>
-        </li>
-        <li class="steps-bullet">
-          <h3 class="steps-subtitle">Step Two</h3>
-          <h4 class="steps-sub-info">Set up a membership for payment options.</h4>
-            <button class="steps-button" type="button">Membership</button>
-        </li>
-        <li class="steps-bullet">
-          <h3 class="steps-subtitle">Step Three</h3>
-          <h4 class="steps-sub-info">Schedule a ride at least 24 hours in advance. 
-            Rides are normally available for any reason, but are limited to medical 
-            appointments and grocery shopping during the Coronavirus pandemic for 
-            your safety</h4>
-        </li>
-      </ul>
+    <div class="steps-header">
+      <h2 class="steps-name">Recieving Rides</h2>
+      <div class="steps-underline"></div>
+    </div>
+    <div class="steps-body">
+      <img class="steps-img"src="<?php echo get_template_directory_uri();?>/images/receiving-rides-steps.png"> 
+      <div class="steps-timeline">
+        <ul class="steps-timeline-list">
+          <li class="steps-bullet">
+            <h3 class="steps-subtitle">Step One</h3>
+            <h4 class="steps-sub-info">Complete an application to ensure we have complete contact information.</h4>
+            <button class="steps-button" type="button">Application</button>
+          </li>
+          <li class="steps-bullet">
+            <h3 class="steps-subtitle">Step Two</h3>
+            <h4 class="steps-sub-info">Set up a membership for payment options.</h4>
+              <button class="steps-button" type="button">Membership</button>
+          </li>
+          <li class="steps-bullet">
+            <h3 class="steps-subtitle">Step Three</h3>
+            <h4 class="steps-sub-info">Schedule a ride at least 24 hours in advance. 
+              Rides are normally available for any reason, but are limited to medical 
+              appointments and grocery shopping during the Coronavirus pandemic for 
+              your safety</h4>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
-
-  </div>
-  
 </div>
 
 
