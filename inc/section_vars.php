933b2fa75b1883a49967775961a6c1ffb85d98fa
llam16 <lydia.lam104@gmail.com>
2021-02-02 23:22:47 -0500

    Added setting/control pairs for About Us

--- inc/section_vars.php
+++ inc/section_vars.php
@@ -3,4 +3,10 @@
   $home_top_vid = 'top_vid';
   $home_top_img = 'home_top_img';
   $home_top_desc = 'home_top_desc';
+  $about_us_section = 'about_us_section';
+  $about_us_top_desc = 'about_us_top_desc';
+  $about_us_quote = 'about_us_quote';
+  $about_us_video = 'about_us_video';
+  $about_us_mid_paragraph = 'about_us_mid_paragraph';
+  $about_us_image_text = 'about_us_image_text';
 ?>
