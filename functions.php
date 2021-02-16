8df8eb9938ebac7552a95e334211bff58b6d0c59
llam16 <lydia.lam104@gmail.com>
2021-02-15 20:37:56 -0500

    added register settings

--- functions.php
+++ functions.php
@@ -2,5 +2,6 @@
   require get_template_directory() . '/inc/example-post-type.php';
   require get_template_directory() . '/inc/enqueue-scripts.php';
   require get_template_directory() . '/inc/register-settings.php';
+  require get_template_directory() . '/inc/register-settings-about-us.php';
 ?>
 
--- inc/section_vars.php
+++ inc/section_vars.php
@@ -3,10 +3,14 @@
   $home_top_vid = 'top_vid';
   $home_top_img = 'home_top_img';
   $home_top_desc = 'home_top_desc';
-  $about_us_section = 'about_us_section';
+  $about_us_top_section = 'about_us_top_section';
+  $about_us_middle_section = 'about_us_middle_section';
+  $about_us_bottom_section = 'about_us_bottom_section';
   $about_us_top_desc = 'about_us_top_desc';
   $about_us_quote = 'about_us_quote';
   $about_us_video = 'about_us_video';
   $about_us_mid_paragraph = 'about_us_mid_paragraph';
   $about_us_image_text = 'about_us_image_text';
+  $about_us_image = 'about_us_image';
+  $about_us_panel = 'about_us_panel';
 ?>
