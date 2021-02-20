fa93fb08fa88b807f29bc950bf558cee28c48028
Alexis Honzik <ahonzik@umich.edu>
2021-02-19 23:50:53 -0500

    Add example repeater and font

--- header.php
+++ header.php
@@ -9,6 +9,8 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet"> 
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com"> 
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
   <title>Independence Rides</title>
 </head>
 
